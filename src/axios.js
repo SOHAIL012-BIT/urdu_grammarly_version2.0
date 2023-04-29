443f36d39ea54aa4e4c644c7398f65fa4146676b
SOHAIL012-BIT <58440518+SOHAIL012-BIT@users.noreply.github.com>
2023-04-30 02:48:04 +0500

    --Axios Setup

---
+++ src/axios.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+import {BaseUrl} from "./config"
+
+const axiosInstance = axios.create({
+    baseURL: BaseUrl,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+
+  // Sending Request 
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      debugger
+     
+      var localStorage = getLocalTokens()
+      if (localStorage !== null) {
+        // config.headers["Authorization"] = 'Bearer ' + localStorage.token;  // for Spring Boot back-end
+      //   config.headers["x-access-token"] = localStorage.token; // for Node.js Express back-end
+      config.headers = { 
+        // 'Authorization': `Bearer ${localStorage.token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+
+//   Accepting response
+
+  axiosInstance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+        debugger
+      const originalConfig = err.config;
+      console.log("err", err)
+    //   if (originalConfig.url !== "/login" && originalConfig !== "/api/User/Logout"  && err.response) {
+    //     // Access Token was expired
+    //     if (err.response.status === 401 && !originalConfig._retry) {
+    //     // if (err.response.status === 401) {
+    //       originalConfig._retry = true;
+  
+    //       try {
+  
+    //           debugger
+  
+    //           const {userid} = getLocalStorage();
+    //           const {token, refreshToken} = getLocalTokens();
+  
+    //           var model = {
+    //               accessToken: token,
+    //               refreshToken: refreshToken,
+    //               usR_ID: userid
+    //           } 
+  
+    //           RefreshToken(model).then((rs) => {
+    //               debugger
+    //               var {message, data} = rs.data
+    //               if(message !== "Token not found"){
+    //                 UpdateTokens(data.accessToken,data.refreshToken)
+    //                 axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.accessToken;
+    //               }
+    //               return axiosInstance(originalConfig);
+    //               // TokenService.updateLocalAccessToken(accessToken);
+    //           }).catch((error) => {
+    //             console.log(error)
+    //             return axiosInstance(originalConfig);
+    //         })
+          
+    //       } 
+    //       catch (_error) {
+    //         return Promise.reject(_error);
+    //       }
+    //     }
+    //   }
+  
+      return Promise.reject(err);
+    }
+  );
+
+
+  export default axiosInstance;
