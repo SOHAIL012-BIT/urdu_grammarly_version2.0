d47f45307c258f39962b153a5a68a2e2e5f8562b
SOHAIL012-BIT <58440518+SOHAIL012-BIT@users.noreply.github.com>
2023-05-02 01:45:30 +0500

    --API Consumption

--- src/sections/auth/signup/SignupForm.js
+++ src/sections/auth/signup/SignupForm.js
@@ -1,27 +1,29 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // @mui
-import { Stack, IconButton, InputAdornment, TextField,FormControl  } from '@mui/material';
+import { Stack, IconButton, InputAdornment, TextField, FormControl } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // components
 import { toaster } from '../../../utils/toaster';
 import Iconify from '../../../components/iconify';
-// API Import 
-import {UserRegister} from '../../../services/AuthenticationServices'
+// API Import
+import { UserRegister } from '../../../services/AuthenticationServices';
 // ----------------------------------------------------------------------
-const InitialState = {
-  username: "",
-  email: "",
-  password: ""
-}
+// const InitialState = {
+//   username: '',
+//   email: '',
+//   password: '',
+// };
+const InitialState = { username:'', email:'', password:'' }
+
+
 export default function SignupForm() {
   const navigate = useNavigate();
   const [state, setState] = useState(InitialState);
   const [showPassword, setShowPassword] = useState(false);
-  
 
   const handleClick = () => {
-    console.log("User data is", state)
+    console.log('User data is', state);
     Register();
     // navigate('/dashboard', { replace: true });
   };
@@ -29,34 +31,62 @@ export default function SignupForm() {
     const temp = { ...state };
     temp[name] = value;
     setState(temp);
-  }
+  };
   // Api Call Function
-  const Register=()=>{
-   
-   UserRegister(state).then(  ({ data }) => {
-          if (data.statusText==="Created") {
-              navigate("/login");
-          } else {
-             toaster(data.statusText, "error")
-          }
-        })
-        .catch(error => {
-           toaster("Something went wrong", "error")
-        })
-  }
+  //   const Register=()=>{
+  // // eslint-disable-next-line no-debugger
+  // debugger
+  //    UserRegister(state).then(  ({ data }) => {
+  //           if (data.statusText==="Created") {
+  //               navigate("/login");
+  //           } else {
+  //              toaster(data.statusText, "error")
+  //           }
+  //         })
+  //         .catch(error => {
+  //            toaster("Something went wrong", "error")
+  //         })
+  //   }
 
 
-  
+  const Register = () => {
+    // Send the form data to the backend API
+    // eslint-disable-next-line no-debugger
+   debugger
+    fetch('http://127.0.0.1:8000/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(state)
+    })
+    .then(response => {
+      // eslint-disable-next-line no-debugger
+      debugger
+      console.log("APi Response is",response);
+      const user=response.json();
+      console.log("User is",user);
+      if(response.statusText==="Created"){
+        toaster("Account Created Successfully", "success")
+        navigate("/login");
+      }
+      else if(response.statusText==="Bad Request"){
+        toaster("Unable to Create account. User name already exist", "error")
+      }
+    })
+    .catch(error =>{
+      toaster("Something went wrong", "error")
+    });
+  };
+
   return (
     <>
       <Stack spacing={3}>
+        <FormControl required>
+          <TextField name="username" label="User Name" onChange={handleChange} required />
+        </FormControl>
 
-      <FormControl required>
-      <TextField name="name" label="User Name"  onChange={handleChange} required/>
-      </FormControl>
-
-
-        <TextField name="email" label="Email address" type='email' onChange={handleChange} required/>
+        <TextField name="email" label="Email address" type="email" onChange={handleChange} required />
 
         <TextField
           name="password"
@@ -68,7 +98,7 @@ export default function SignupForm() {
             endAdornment: (
               <InputAdornment position="end">
                 <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                <Iconify icon={showPassword ?'eva:eye-off-fill':'ic:round-remove-red-eye'} />
+                  <Iconify icon={showPassword ? 'eva:eye-off-fill' : 'ic:round-remove-red-eye'} />
                 </IconButton>
               </InputAdornment>
             ),
@@ -81,22 +111,26 @@ export default function SignupForm() {
         <Link variant="subtitle2" underline="hover"  style={{color:"#323439"}}>
           Forgot password?
         </Link> */}
-        <p
-                    className="text-center mb-3 mt-3 text-muted small"
-                    style={{ fontSize: "14px" }}
-                  >
-                    By signing up, you agree to the{" "}
-                    <a href="#action" style={{ textDecoration: "underline" }}>
-                      Terms and Conditions{" "}
-                    </a>{" "}
-                    and{" "}
-                    <a href="#act" style={{ textDecoration: "underline" }}>
-                      Privacy Policy.
-                    </a>{" "}
-                  </p>
+        <p className="text-center mb-3 mt-3 text-muted small" style={{ fontSize: '14px' }}>
+          By signing up, you agree to the{' '}
+          <a href="#action" style={{ textDecoration: 'underline' }}>
+            Terms and Conditions{' '}
+          </a>{' '}
+          and{' '}
+          <a href="#act" style={{ textDecoration: 'underline' }}>
+            Privacy Policy.
+          </a>{' '}
+        </p>
       </Stack>
 
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}  style={{backgroundColor:"#323439", color:"#ffffff"}}>
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        onClick={handleClick}
+        style={{ backgroundColor: '#323439', color: '#ffffff' }}
+      >
         Agree & Sign up
       </LoadingButton>
     </>
--- src/services/AuthenticationServices.js
+++ src/services/AuthenticationServices.js
@@ -6,5 +6,7 @@ export const UserLogin = (model) => {
     return axiosInstance.post(`/auth/`, model);
 }
 export const UserRegister = (model) => {
+    // eslint-disable-next-line no-debugger
+debugger
     return axiosInstance.post(`/api/users/`, model);
 }
