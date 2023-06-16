cb79828f95bc1f79f114a9d3663f6c801c9c8914
SOHAIL012-BIT <58440518+SOHAIL012-BIT@users.noreply.github.com>
2023-06-16 23:01:28 +0500

    ---Header footer setup

--- src/websitepages/AboutUs.js
+++ src/websitepages/AboutUs.js
@@ -7,6 +7,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import aboutUs from '../assets/images/aboutus.png'
 import logo from '../assets/images/dashboardLogo.png'
 
+import Header from './Header'
+import Footer from './Footer'
+
 const useStyles = makeStyles((theme) => ({
     banner: {
         // background: `linear-gradient(to right, #f5f5f5, #ffffff)`,
@@ -20,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
     bannerText: {
         fontSize: '32pt',
         fontWeight: 'bold',
-        color:"#ffffff",
+        color: "#ffffff",
         marginRight: theme.spacing(2),
     },
     bannerImage: {
@@ -29,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
     },
     columnContainer: {
         marginBottom: theme.spacing(4),
-        color:"#1f243c"
+        color: "#1f243c"
     },
     subtitle: {
         fontStyle: 'italic',
@@ -40,10 +43,10 @@ const useStyles = makeStyles((theme) => ({
         // fontSize: "14pt",
         color: "#1f243c",
         fontFamily: "'Google Sans', sans-serif",
-        fontWeight:"bold"
+        fontWeight: "bold"
     },
-    bannerDetail:{
-        backgroundColor:"#ffffff"
+    bannerDetail: {
+        backgroundColor: "#ffffff"
     }
 }));
 
@@ -52,7 +55,7 @@ function About() {
 
     return (
         <>
-
+            <Header />
             <div className={classes.banner}>
                 <Container maxWidth="lg" style={{ display: 'flex', alignItems: 'center' }}>
                     <Grid container spacing={4}>
@@ -75,45 +78,45 @@ function About() {
                 </Container>
             </div >
             <div className={classes.bannerDetail}>
-            <Container maxWidth="lg" className={classes.columnContainer}>
-              
-                <Grid container spacing={4} justifyContent="center">
-                <Grid item xs={12} sm={6}>
-                <Typography variant="h3" component="h2" gutterBottom style={{textAlign:"center"}} className={classes.introText}>
-                Improve lives by improving communication
-                </Typography>
-                <br />
-                <Typography variant="subtitle1" component="h2" gutterBottom style={{textAlign:"center"}} className={classes.introText}>
-                We’ve climbed a long way — with much farther to the summit
-                </Typography>
-                </Grid>
-                </Grid>
-</Container>
-            <Container maxWidth="lg" className={classes.columnContainer}>
-              
-                <Grid container spacing={4} justifyContent="center">
-                    <Grid item xs={12} sm={6}>
-                      
-                        <Typography variant="body1" align="justify" className={classes.introText}>
-                            Urdu is our national language and a source of pride for us. With the goal of encouraging Urdu writing and the use of Urdu in everyday speech, we provide Grammar Check, Sentence Correction, and Word Suggestions. At Urdu Grammarly, we seek to enable individuals to communicate in Urdu with one another whenever and wherever they can. Grammarly promotes efficient and productive writing. Our ideas assist in identifying and replacing complex phrases with more efficient ones, refreshing repetitious terminology, and maintaining correct spelling, punctuation, and grammar.
-                        </Typography>
-                        <Typography variant="body1" align="justify" style={{ marginTop: "1rem" }} className={classes.introText}>
-                            We founded Urdu Grammarly with the goal of helping people communicate more effectively.Our app shall help the Urdu Columnists, Social Scientists, Novelists, News Reporters, and Urdu writers being the ultimate custodian of this language.Urdu Grammarly shall create a setup that ease to write Urdu for its every user Urdu.Urdu Grammarly has grown the capabilities of our writing assistance to go way beyond grammar and spelling to analyze complex aspects of language and communication.
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Typography variant="body1" align="justify" style={{ textAlign: "center", direction: "rtl" }} className={classes.introText}>
-                            اردو ہماری قومی زبان ہے اور ہمارے لئے فخر کا باعث ہے۔ اردو لکھنے اور ہر روزہ گفتگو میں اردو کے استعمال کی ترویج کے مقصد کے ساتھ ہم گرامر چیک، جملہ میں تصحیح اور الفاظ کی تجاویز فراہم کرتے ہیں۔ ہم یوں چاہتے ہیں کہ افراد اردو میں بات چیت کریں جب بھی اور جہاں بھی وہ چاہیں۔ گرامرلی موثر اور پیداوارکار تحریر کو فروغ دیتی ہے۔ ہمارے خیالات مدد کرتے ہیں کہ مشکل عبارات کو مددگر الفاظ سے تبدیل کیا جائے، تکرار کے الفاظ کو تازہ کیا جائے، درست ہجے، علامات وقفے اور گرامر کی حفاظت رکھی جائے۔
-                        </Typography>
-                        <Typography variant="body1" align="justify" style={{ marginTop: "1rem", textAlign: "center", direction: "rtl" }} className={classes.introText}>
-                            ہم نے اردو گرامرلی کی بنیاد اس مقصد کے ساتھ رکھی ہے کہ لوگوں کو زیادہ مؤثر طریقے سے بات چیت کرنے میں مدد ملے۔ ہماری ایپ اردو کالم نگاروں، سماجی سائنسدانوں، ناول نگاروں، نیوز رپورٹروں، اور اردو مصنفین کو اس زبان کے حتمی محافظ ہونے میں مدد دے گی۔ اپنے ہر صارف کے لیے اردو لکھیں۔ اردو گرامر نے زبان اور مواصلات کے پیچیدہ پہلوؤں کا تجزیہ کرنے کے لیے گرامر اور املا سے آگے بڑھنے کے لیے ہماری تحریری معاونت کی صلاحیتوں میں اضافہ کیا ہے۔
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Container>
-</div>
+                <Container maxWidth="lg" className={classes.columnContainer}>
 
+                    <Grid container spacing={4} justifyContent="center">
+                        <Grid item xs={12} sm={6}>
+                            <Typography variant="h3" component="h2" gutterBottom style={{ textAlign: "center" }} className={classes.introText}>
+                                Improve lives by improving communication
+                            </Typography>
+                            <br />
+                            <Typography variant="subtitle1" component="h2" gutterBottom style={{ textAlign: "center" }} className={classes.introText}>
+                                We’ve climbed a long way — with much farther to the summit
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
+                <Container maxWidth="lg" className={classes.columnContainer}>
 
+                    <Grid container spacing={4} justifyContent="center">
+                        <Grid item xs={12} sm={6}>
+
+                            <Typography variant="body1" align="justify" className={classes.introText}>
+                                Urdu is our national language and a source of pride for us. With the goal of encouraging Urdu writing and the use of Urdu in everyday speech, we provide Grammar Check, Sentence Correction, and Word Suggestions. At Urdu Grammarly, we seek to enable individuals to communicate in Urdu with one another whenever and wherever they can. Grammarly promotes efficient and productive writing. Our ideas assist in identifying and replacing complex phrases with more efficient ones, refreshing repetitious terminology, and maintaining correct spelling, punctuation, and grammar.
+                            </Typography>
+                            <Typography variant="body1" align="justify" style={{ marginTop: "1rem" }} className={classes.introText}>
+                                We founded Urdu Grammarly with the goal of helping people communicate more effectively.Our app shall help the Urdu Columnists, Social Scientists, Novelists, News Reporters, and Urdu writers being the ultimate custodian of this language.Urdu Grammarly shall create a setup that ease to write Urdu for its every user Urdu.Urdu Grammarly has grown the capabilities of our writing assistance to go way beyond grammar and spelling to analyze complex aspects of language and communication.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Typography variant="body1" align="justify" style={{ textAlign: "center", direction: "rtl" }} className={classes.introText}>
+                                اردو ہماری قومی زبان ہے اور ہمارے لئے فخر کا باعث ہے۔ اردو لکھنے اور ہر روزہ گفتگو میں اردو کے استعمال کی ترویج کے مقصد کے ساتھ ہم گرامر چیک، جملہ میں تصحیح اور الفاظ کی تجاویز فراہم کرتے ہیں۔ ہم یوں چاہتے ہیں کہ افراد اردو میں بات چیت کریں جب بھی اور جہاں بھی وہ چاہیں۔ گرامرلی موثر اور پیداوارکار تحریر کو فروغ دیتی ہے۔ ہمارے خیالات مدد کرتے ہیں کہ مشکل عبارات کو مددگر الفاظ سے تبدیل کیا جائے، تکرار کے الفاظ کو تازہ کیا جائے، درست ہجے، علامات وقفے اور گرامر کی حفاظت رکھی جائے۔
+                            </Typography>
+                            <Typography variant="body1" align="justify" style={{ marginTop: "1rem", textAlign: "center", direction: "rtl" }} className={classes.introText}>
+                                ہم نے اردو گرامرلی کی بنیاد اس مقصد کے ساتھ رکھی ہے کہ لوگوں کو زیادہ مؤثر طریقے سے بات چیت کرنے میں مدد ملے۔ ہماری ایپ اردو کالم نگاروں، سماجی سائنسدانوں، ناول نگاروں، نیوز رپورٹروں، اور اردو مصنفین کو اس زبان کے حتمی محافظ ہونے میں مدد دے گی۔ اپنے ہر صارف کے لیے اردو لکھیں۔ اردو گرامر نے زبان اور مواصلات کے پیچیدہ پہلوؤں کا تجزیہ کرنے کے لیے گرامر اور املا سے آگے بڑھنے کے لیے ہماری تحریری معاونت کی صلاحیتوں میں اضافہ کیا ہے۔
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
+
+            <Footer/>
         </>
     );
 }
